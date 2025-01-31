9d3c5326956226c794db52140cbd4656d0f44779
AnnelineD <anneline.daggelinckx@hotmail.com>
2025-01-31 12:20:33 +0100

    Add pattern generation test case with offset

--- src/pattern.py
+++ src/pattern.py
@@ -23,15 +23,11 @@ def minimal_seq(xs: list[int]) -> list[int]:
 
 
 def pattern_ext(n, offset, base):
-  # BASE10: pattern_(3) = [0, 3, 6, 9, 2, 5, 8, 1, 4, 7]
-  # BASE10: pattern_(7) = [0, 7, 4, 1, 8, 5, 2, 9, 6, 3]
-  # BASE10: pattern_(15) = [0, 15, 30, 45, 60, 75, 90, 5, 20, 35, 50, 65, 80, 95, 10, 25, 40, 55, 70, 85]
-  # BASE10: pattern_(89) = [0, 89, 78, 67, 56, 45, 34, 23, 12, 1, 90, 79, 68, 57, 46, 35, 24, 13, 2, 91, 80, 69, 58, 47, 36, 25, 14, 3, 92, 81, 70, 59, 48, 37, 26, 15, 4, 93, 82, 71, 60, 49, 38, 27, 16, 5, 94, 83, 72, 61, 50, 39, 28, 17, 6, 95, 84, 73, 62, 51, 40, 29, 18, 7, 96, 85, 74, 63, 52, 41, 30, 19, 8, 97, 86, 75, 64, 53, 42, 31, 20, 9, 98, 87, 76, 65, 54, 43, 32, 21, 10, 99, 88, 77, 66, 55, 44, 33, 22, 11]
   if n == 0:
       return [offset]
 
   assert offset >= 0
-  assert offset < n
+  assert offset < n     # if offset >= n, we can get a pattern (e.g. pattern_ext(3, 3, 10) = [3, 6, 9, 2, 5, 8, 1, 4, 7, 0]) such that the first and last element of the pattern belong to the same group
 
   order = len(numberToBase(n, base))
   return [(k + offset) % (base ** order) for k in range(0, lcm(base ** order, n), n)]
