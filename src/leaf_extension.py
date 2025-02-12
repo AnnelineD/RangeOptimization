d529e1893908dbd71661fb4626659ea32e93a01e
AnnelineD <anneline.daggelinckx@hotmail.com>
2025-02-12 14:16:57 +0100

    no minimal sequence over pattern: pattern already returns minimal sequence

--- src/leaf_extension.py
+++ src/leaf_extension.py
@@ -9,7 +9,7 @@ def make_leaf_nodes(start, step, base, l):
   assert start < base
   assert step < base
 
-  pat = minimal_seq(pattern(step, start, base))
+  pat = pattern(step, start, base)
   return [Node({p: ()}, l) for p in pat]
 
 
@@ -17,28 +17,24 @@ def next_step(start_split, step_split, prev_nodes, base, l):
   # assert step < base
   # assert start < base
 
-  lv_new = []
   step_o = to_number(step_split[1:], base)
 
   if step_o == 0:
-    pat = minimal_seq(pattern(step_split[0], start_split[0], base))
+    pat = pattern(step_split[0], start_split[0], base)
   else:
-    pat_full = minimal_seq(pattern(to_number(step_split, base), to_number(start_split, base), base**len(step_split)))
+    pat_full = pattern(to_number(step_split, base), to_number(start_split, base), base**len(step_split))
     pat = [to_size(numberToBase(i, base), len(step_split))[0] for i in pat_full]
 
   lv_prev_it = iter(cycle(prev_nodes))
 
-  for p in pat:
-    lv_new.append(Node({p: next(lv_prev_it)}, l)) # kan het hier dan nooit dat een node meermaals moet verbinden???
-
-  return lv_new
+  return [Node({p: next(lv_prev_it)}, l) for p in pat]
 
 
 def last_layer(start_split, step_split, prev_nodes, base, l):
     lv_new = []
     lv_prev_it = iter(cycle(prev_nodes))
 
-    pat_full = minimal_seq(pattern(to_number(step_split, base), to_number(start_split, base), base ** len(step_split)))
+    pat_full = pattern(to_number(step_split, base), to_number(start_split, base), base ** len(step_split))
 
     r = repetition_offset(to_number(step_split, base), to_number(start_split, base), base ** len(step_split))
     if len(pat_full) == sum(minimal_seq(r)):
